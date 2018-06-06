e834bfa9b05ba425b5cec361322b675e0209ef95
ahribori <ahribori@gmail.com>
2018-06-07 00:45:43 +0900

    구글 시트 API 예제 추가 #2

--- src/containers/GoogleSheet.js
+++ src/containers/GoogleSheet.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Script from 'react-load-script';
+import styled from 'styled-components';
 
 const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
 const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY || '';
 const DISCOVERY_DOCS = ['https://sheets.googleapis.com/$discovery/rest?version=v4'];
-const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
 
 class GoogleSheet extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: null,
+        };
     }
 
     initClient = () => {
@@ -34,11 +38,41 @@ class GoogleSheet extends React.Component {
         if (isSignedIn) {
             authorizeButton.style.display = 'none';
             signoutButton.style.display = 'block';
-            // listMajors();
+            this.getSheet();
         } else {
             authorizeButton.style.display = 'block';
             signoutButton.style.display = 'none';
         }
+    };
+
+    getSheet = () => {
+        window.gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: '1bAR5Dh3QWiXDYB0_gaTBDTd6WkutBKO3Q5VVb5W0WGA',
+            range: '시트1!A1:C5',
+        }).then((response) => {
+            const { values } = response.result;
+            this.setState({ data: values });
+        }, (error) => {
+            console.error(error.result.error.message);
+        });
+    };
+
+    updateCell = () => {
+        window.gapi.client.sheets.spreadsheets.values.update({
+            spreadsheetId: '1bAR5Dh3QWiXDYB0_gaTBDTd6WkutBKO3Q5VVb5W0WGA',
+            range: '시트1!A6',
+            valueInputOption: 'USER_ENTERED',
+        },{
+            "range": '시트1!A6',
+            "majorDimension": 'COLUMNS',
+            "values": [
+                [1]
+            ]
+        }).then((response) => {
+            console.log(response);
+        }, (error) => {
+            console.error(error.result.error.message);
+        });
     };
 
     handleAuthClick = () => {
@@ -56,6 +90,22 @@ class GoogleSheet extends React.Component {
     onScriptError = () => {
     };
 
+    renderSheet = () => {
+        if (!this.state.data) {
+            return null;
+        } else {
+            return this.state.data.map(row => {
+                return (
+                    <div key={row}>
+                        {!Array.isArray(row) ? (<span className={this.props.className}>{row}</span>) : row.map(col => {
+                            return <span key={col} className={this.props.className}>{col}</span>;
+                        })}
+                    </div>
+                );
+            });
+        }
+    };
+
     render() {
         return (
             <div>
@@ -66,12 +116,19 @@ class GoogleSheet extends React.Component {
                     onError={this.onScriptError}
                 />
                 <h1>Google Sheet</h1>
-                <button id="authorize-button" style={{display: 'none'}}>Authorize</button>
-                <button id="signout-button" style={{display: 'none'}}>Sign Out</button>
+                <button id="authorize-button" style={{ display: 'none' }}>Authorize</button>
+                <button id="signout-button" style={{ display: 'none' }}>Sign Out</button>
+                <button id="update-button" onClick={this.updateCell}>Update Cell</button>
+                <hr/>
+                {this.renderSheet()}
             </div>
         );
     }
-
 }
 
-export default GoogleSheet;
+const StyledGoogleSheet = styled(GoogleSheet)`
+  margin: 10px;
+  border: 1px dashed gray;
+`;
+
+export default StyledGoogleSheet;
