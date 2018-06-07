345a2927bf1781c33a0197ec1d68c691c4d10d35
ahribori <ahribori@gmail.com>
2018-06-08 00:20:09 +0900

    Add fullcalendar js

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import $ from 'jquery';
+import moment from 'moment';
+import 'fullcalendar';
+import 'fullcalendar/dist/fullcalendar.min.css';
 
 class Calendar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-        <div>Calendar</div>
-    )
-  }
+    componentDidMount() {
+        const $calendar = $('#calendar');
+        $calendar.fullCalendar({
+            height: $(window).height() * 0.83,
+            windowResize: () => {
+                $calendar.fullCalendar('option', 'height', $(window).height() * 0.83);
+            },
+        });
+
+    }
+
+    render() {
+        return (
+            <div id={'calendar'}></div>
+        );
+    }
 }
 
 export default Calendar;
