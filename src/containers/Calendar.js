c26186cecc823c9d9c8040faa42d5b084adb6db0
ahribori <ahribori@gmail.com>
2018-06-09 00:18:01 +0900

    add events to calendar
    add lots of event

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -16,6 +16,63 @@ class Calendar extends React.Component {
             windowResize: () => {
                 $calendar.fullCalendar('option', 'height', $(window).height() * 0.83);
             },
+            eventSources: [
+                {
+                    color: 'blue',
+                    textColor: 'yellow',
+                    events: [
+                        {
+                            title  : 'event1',
+                            start  : '2018-06-09'
+                        },
+                        {
+                            title  : 'event2',
+                            start  : '2018-06-11',
+                            end    : '2018-06-15'
+                        },
+                        {
+                            title  : 'event3',
+                            start  : '2018-06-19T12:30:00',
+                            allDay : false // will make the time show
+                        }
+                    ]
+                },
+                {
+                    color: 'red',
+                    textColor: 'white',
+                    events: [
+                        {
+                            title  : 'event4',
+                            start  : '2018-06-25'
+                        },
+                        {
+                            title  : 'event5',
+                            start  : '2018-06-25'
+                        },
+                        {
+                            title  : 'event6',
+                            start  : '2018-06-25'
+                        },
+                        {
+                            title  : 'event7',
+                            start  : '2018-06-25'
+                        },
+                        {
+                            title  : 'event8',
+                            start  : '2018-06-25'
+                        },
+                        {
+                            title  : 'event9',
+                            start  : '2018-06-25'
+                        },
+                        {
+                            title  : 'event10',
+                            start  : '2018-06-25'
+                        },
+                    ]
+                },
+
+            ]
         });
 
     }
