60538664c828c493410a6f0965f59ec8f0849cb7
ahribori <ahribori@gmail.com>
2018-06-17 16:27:08 +0900

    캘린더 이벤트 등록 Dialog

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -1,17 +1,59 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+import styled from 'styled-components';
 import $ from 'jquery';
 import moment from 'moment';
 import 'fullcalendar';
 import 'fullcalendar/dist/fullcalendar.min.css';
 
+const Dialog = styled(({ className, title, show, onSubmit, onClose, handleChange }) => (
+    <div className={className}>
+        <div className="dialog">
+            <input
+                type="text"
+                value={title}
+                onChange={e => {
+                    handleChange(e.target.value);
+                }}
+            />
+            <button onClick={onSubmit}>등록</button>
+            <button onClick={onClose}>닫기</button>
+        </div>
+    </div>
+))`
+  display: ${ props => props.show ? 'flex' : 'none' }
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height:100%;
+  background-color: rgba(0,0,0,0.5);
+  z-index: 100;
+  & .dialog {
+    position: relative;
+    padding: 20px;
+    background-color: white;
+    box-shadow: #222222 1px 1px 4px;
+    width: 400px;
+    height: 400px;
+  }
+`;
+
 class Calendar extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            dialogShow: false,
+            title: '',
+            start: null,
+            end: null,
+        };
     }
 
-    addEvent = (start, end) => {
+    addEvent = (title, start, end) => {
         $('#calendar').fullCalendar('renderEvent', {
-            title: 'selected!',
+            title: title,
             start,
             end,
         });
@@ -25,12 +67,17 @@ class Calendar extends React.Component {
                 return window.innerHeight - 30;
             },
             windowResize: () => {
-               $calendar.fullCalendar('option', 'height', window.innerHeight - 30);
+                $calendar.fullCalendar('option', 'height', window.innerHeight - 30);
             },
             windowResizeDelay: 0,
             selectable: true,
             select: (start, end, event, view) => {
-                this.addEvent(start, end);
+                this.setState({
+                    dialogShow: true,
+                    title: '',
+                    start,
+                    end,
+                });
             },
             eventSources: [
                 {
@@ -95,7 +142,25 @@ class Calendar extends React.Component {
 
     render() {
         return (
-            <div id={'calendar'}></div>
+            <div>
+                {ReactDom.createPortal(
+                    <Dialog
+                        show={this.state.dialogShow}
+                        onSubmit={() => {
+                            this.addEvent(this.state.title, this.state.start, this.state.end);
+                            this.setState({ dialogShow: false });
+                        }}
+                        onClose={() => {
+                            this.setState({ dialogShow: false });
+                        }}
+                        handleChange={title => {
+                            this.setState({ title });
+                        }}
+                        title={this.state.title}
+                    />
+                    , document.body)}
+                <div id={'calendar'}/>
+            </div>
         );
     }
 }
