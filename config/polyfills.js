c4e825065f22426127343db32de60e7105238d41
ahribori <ahribori@gmail.com>
2018-09-14 00:45:08 +0900

    Polyfill test

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,4 +1,6 @@
 'use strict';
+require('@babel/polyfill');
+require('core-js/es6/symbol');
 
 if (typeof Promise === 'undefined') {
   // Rejection tracking prevents a common issue where React gets into an
