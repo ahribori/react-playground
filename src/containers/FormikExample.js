de125752d1f9ffa5fdf3b4a9eff4d6ca2e6911e1
ahribori <ahribori@gmail.com>
2018-12-05 00:16:48 +0900

    validate와 validationSchema 로 Form level validation 해보기

--- src/containers/FormikExample.js
+++ src/containers/FormikExample.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const FormSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('올바른 이메일 형식이 아닙니다.')
+    .required('이메일을 입력하세요.'),
+  name: Yup.string()
+    .min(2, '이름은 2~10 글자 사이로 입력하세요.')
+    .max(10, '이름은 2~10 글자 사이로 입력하세요.')
+    .required('이름을 입력하세요.'),
+  age: Yup.number()
+    .required('나이를 입력하세요.')
+    .typeError('왜 숫자가 아니야~')
+    .positive()
+    .integer('왜 숫자가 아니냐;'),
+});
+
+const validate = values => {
+  const errors = {};
+  const { email, name, age } = values;
+  if (name.length > 10) {
+    errors.name = '열 자 이상 입력 불가'
+  }
+  console.log(errors)
+  return errors;
+};
 
 class FormikExample extends Component {
 
   handleSubmit = props => (e) => {
-    console.log(props)
+    console.log(props);
+    console.log(props.errors);
+  };
 
+  validate = (values) => {
+    console.log(values)
+    const errors = {};
+    if (values.name.length > 10) {
+      errors.name = '이름이 10자가 넘어요!';
+    }
+    return errors;
   };
 
   render() {
@@ -13,16 +48,34 @@ class FormikExample extends Component {
       <div>
         <Formik
           initialValues={{
-            name: 'daniel'
+            email: '',
+            name: '',
+            age: '',
           }}
+          validationSchema={FormSchema}
+          validate={validate}
           render={props => (
             <form action="">
               <input
                 type="text"
                 onChange={props.handleChange}
                 onBlur={props.handleBlur}
+                value={props.values.email}
+                name="email"
+              />
+              <input
+                type="text"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
                 value={props.values.name}
                 name="name"
+              />
+              <input
+                type="text"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.age}
+                name="age"
               />
               <button onClick={this.handleSubmit(props)} type="button">submit</button>
             </form>
