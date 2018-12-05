c762164b3634592482c5b62f318e008372d38bce
ahribori <ahribori@gmail.com>
2018-12-06 00:13:16 +0900

    Form level validation과 Field level validation 사용해보기

--- src/containers/FormikExample.js
+++ src/containers/FormikExample.js
@@ -1,83 +1,117 @@
 import React, { Component } from 'react';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-
-const FormSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('올바른 이메일 형식이 아닙니다.')
-    .required('이메일을 입력하세요.'),
-  name: Yup.string()
-    .min(2, '이름은 2~10 글자 사이로 입력하세요.')
-    .max(10, '이름은 2~10 글자 사이로 입력하세요.')
-    .required('이름을 입력하세요.'),
-  age: Yup.number()
-    .required('나이를 입력하세요.')
-    .typeError('왜 숫자가 아니야~')
-    .positive()
-    .integer('왜 숫자가 아니냐;'),
-});
-
-const validate = values => {
-  const errors = {};
-  const { email, name, age } = values;
-  if (name.length > 10) {
-    errors.name = '열 자 이상 입력 불가'
-  }
-  console.log(errors)
-  return errors;
-};
+import { Formik, Field, ErrorMessage } from 'formik';
 
 class FormikExample extends Component {
+  userInputHooks = next => e => {
+    const { name, value } = e.target;
 
-  handleSubmit = props => (e) => {
-    console.log(props);
-    console.log(props.errors);
+    // Formik form 내부로 input event가 전파되기 전 처리
+
+    next(e);
   };
 
-  validate = (values) => {
-    console.log(values)
+  formLevelValidate = values => {
     const errors = {};
-    if (values.name.length > 10) {
-      errors.name = '이름이 10자가 넘어요!';
+    console.log(values);
+    const { email, name, age } = values;
+    if (name.length > 10) {
+      errors.name = '열 자 이상 입력 불가';
+    }
+    if (
+      !/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/gi.test(
+        values.email,
+      )
+    ) {
+      errors.email = '올바른 이메일 형식이 아닙니다.';
     }
     return errors;
+  };
+
+  validateEmail = value => {
+    let error;
+    if (
+      !/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/gi.test(
+        value,
+      )
+    ) {
+      error = '올바른 이메일 형식이 아닙니다.';
+    }
+    console.log(error);
+    return error;
+  };
+
+  validateName = value => {
+    let error;
+    if (value.length > 10) {
+      error = '이름이 10자가 넘습니다.';
+    }
+    console.log(error);
+    return error;
+  };
+
+  validateAge = value => {
+    console.log('validateAge');
+    let error;
+    if (!/\^\d+$/.test(value)) {
+      error = '숫자가 아닙니다.';
+    }
+    return error;
+  };
+
+  handleSubmit = props => e => {
+    const { errors } = props;
+    Object.keys(errors).forEach(fieldName => {
+      console.error(`[${fieldName}] ${errors[fieldName]}`);
+    });
   };
 
   render() {
     return (
       <div>
         <Formik
-          initialValues={{
-            email: '',
-            name: '',
-            age: '',
-          }}
-          validationSchema={FormSchema}
-          validate={validate}
+          initialValues={{ email: '', name: '', age: '' }}
+          validate={this.formLevelValidate}
           render={props => (
             <form action="">
-              <input
-                type="text"
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                value={props.values.email}
-                name="email"
-              />
-              <input
-                type="text"
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                value={props.values.name}
-                name="name"
-              />
-              <input
-                type="text"
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                value={props.values.age}
-                name="age"
-              />
-              <button onClick={this.handleSubmit(props)} type="button">submit</button>
+              <div>
+                <label htmlFor="email">이메일</label>
+                <Field
+                  type="text"
+                  onChange={this.userInputHooks(props.handleChange)}
+                  onBlur={props.handleBlur}
+                  value={props.values.email}
+                  name="email"
+                  validate={this.validateEmail}
+                />
+                <ErrorMessage name="email" />
+              </div>
+              <div>
+                <label htmlFor="name">이름</label>
+                <Field
+                  type="text"
+                  onChange={this.userInputHooks(props.handleChange)}
+                  onBlur={props.handleBlur}
+                  value={props.values.name}
+                  name="name"
+                  validate={this.validateName}
+                />
+                <ErrorMessage name="name" />
+              </div>
+              <div>
+                <label htmlFor="age">나이</label>
+                <Field
+                  type="text"
+                  onChange={this.userInputHooks(props.handleChange)}
+                  onBlur={props.handleBlur}
+                  value={props.values.age}
+                  name="age"
+                  validate={this.validateAge}
+                />
+                <ErrorMessage name="age" />
+              </div>
+              <button onClick={this.handleSubmit(props)} type="button">
+                submit
+              </button>
             </form>
           )}
         />
