422dce448759868c710cd7da690810748e4e28e0
ahribori <ahribori@gmail.com>
2018-12-07 00:28:37 +0900

    React Hooks 써보자 - 카운터

--- src/containers/ReactHooks.js
+++ src/containers/ReactHooks.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+
+const Counter = ({}) => {
+  const [value /* 값 */, setValue /* 값을 변화시킬 수 있는 setter */] = useState(0 /* 초기값 */);
+
+  return (
+    <div>
+      <p>
+        <b>{value}</b>번 누르셨습니다.
+      </p>
+      <button onClick={() => setValue(value + 1)}>+1</button>
+      <button onClick={() => setValue(value - 1)}>-1</button>
+    </div>
+  );
+};
 
 class ReactHooks extends Component {
   render() {
     return (
       <div>
-        RH
+        <Counter />
       </div>
     );
   }
