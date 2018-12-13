387c7f3cfb3d29f9b58ce9705bad7f50c198d2ab
ahribori <ahribori@gmail.com>
2018-12-14 00:48:48 +0900

    Draggable한 object 만들어보기

---
+++ src/components/ReactDnD/Toy.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { DragSource } from 'react-dnd';
+
+const Types = {
+  ITEM: 'toy',
+};
+const itemSource = {
+  beginDrag(props) {
+    return {
+      id: props.id
+    }
+  },
+  endDrag(props) {
+    console.log(props)
+    /* code here */
+  },
+  isDragging(props, monitor) {
+    console.log(props, monitor)
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging(),
+  };
+}
+
+class Toy extends Component {
+  render() {
+    const { isDragging, connectDragSource, src } = this.props;
+    return connectDragSource(
+      <div style={{ border: '1px solid red', width: 50, height: 50 }}/>,
+      /* code here */
+    );
+  }
+}
+
+export default DragSource(Types.ITEM, itemSource, collect)(Toy);
--- src/containers/ReactDnD.js
+++ src/containers/ReactDnD.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContext } from 'react-dnd';
+import Toy from '../components/ReactDnD/Toy';
 
 class ReactDnD extends Component {
   render() {
     return (
       <div>
-        ReactDnD
+        <Toy id={12} />
       </div>
     );
   }
 }
 
-export default ReactDnD;
+export default DragDropContext(HTML5Backend)(ReactDnD);
