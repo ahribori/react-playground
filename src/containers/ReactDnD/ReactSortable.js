2c850450edc4dbe96dfd917fc8fb2948120c66b2
ahribori <ahribori@gmail.com>
2018-12-19 00:06:56 +0900

    React Sortable HOC Example

--- src/containers/ReactDnD/ReactSortable.js
+++ src/containers/ReactDnD/ReactSortable.js
@@ -1,196 +1,53 @@
 import React, { Component } from 'react';
-import HTML5Backend from 'react-dnd-html5-backend';
-import { DragDropContext, DragSource, DropTarget } from 'react-dnd';
+import {
+  SortableContainer,
+  SortableElement,
+  arrayMove,
+} from 'react-sortable-hoc';
 
-const typeCard = '@@Type::Card';
-
-const containerStyle = {};
-
-const cardStyle = {
-  margin: 5,
-  height: 50,
+const styles = {
+  ul: {
+    padding: 0,
+  },
+  li: {
+    height: 30,
+    boxShadow: '1px 1px 1px gray',
+    margin: 15,
+    padding: 15,
+    listStyleType: 'none',
+    backgroundColor: 'white',
+    cursor: 'row-resize'
+  }
 };
 
-const Card = props => {
-  const style = props.isDragging
-    ? Object.assign({}, cardStyle, {
-        backgroundColor: props.color,
-        opacity: 0.5,
-      })
-    : Object.assign({}, cardStyle, { backgroundColor: props.color });
+const SortableItem = SortableElement(({ value }) => <li style={styles.li}>{value}</li>);
 
-  return props.connectDropTarget(
-    <article style={containerStyle}>
-      {props.connectDragSource(
-        <div style={style}>
-          <div className="card-body">
-            <h5>{props.title}</h5>
-          </div>
-        </div>,
-      )}
-    </article>,
+const SortableList = SortableContainer(({ items }) => {
+  return (
+    <ul style={styles.ul}>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${index}`} index={index} value={value} />
+      ))}
+    </ul>
   );
-};
-
-// const CustomDragLayer = ({ item, itemType, isDragging, ...props }) => {
-//   const layerStyles = {
-//     position: 'fixed',
-//     pointerEvents: 'none',
-//     zIndex: 100,
-//     left: 0,
-//     top: 0,
-//     width: '100%',
-//     height: '100%',
-//   };
-//
-//   function getItemStyles(props) {
-//     const { currentOffset } = props;
-//     if (!currentOffset) {
-//       return {
-//         display: 'none',
-//       };
-//     }
-//
-//     const { x, y } = currentOffset;
-//     const transform = `translate(${x}px, ${y}px)`;
-//     return {
-//       transform: transform,
-//       WebkitTransform: transform,
-//     };
-//   }
-//
-//   if (!isDragging) {
-//     return null;
-//   }
-//
-//   function renderItem(type, item) {
-//     switch (type) {
-//       case typeCard:
-//         return <div>프리뷰!</div>;
-//       default:
-//     }
-//   }
-//   return (
-//     <div style={layerStyles}>
-//       <div style={getItemStyles(props)}>{renderItem(itemType, item)}</div>
-//     </div>
-//   );
-// };
-
-const specTarget = {
-  drop(props) {
-    return {
-      id: props.id,
-      index: props.index,
-    };
-  },
-  hover(props, monitor, component) {},
-};
-
-const specSource = {
-  beginDrag(props) {
-    return {
-      id: props.id,
-    };
-  },
-  endDrag(props, monitor) {
-    if (!monitor.didDrop()) {
-      return;
-    }
-    const source = monitor.getItem();
-    const target = monitor.getDropResult();
-
-    if (source.id === target.id) {
-      return;
-    }
-    props.moveCard(source.id, target.index);
-  },
-};
-
-const collectTarget = (connect, monitor) => ({
-  connectDropTarget: connect.dropTarget(),
-  isOver: monitor.isOver(),
 });
-
-const collectSource = (connect, monitor) => ({
-  connectDragSource: connect.dragSource(),
-  isDragging: monitor.isDragging(),
-});
-
-// const collectLayer = monitor => {
-//   return {
-//     item: monitor.getItem(),
-//     itemType: monitor.getItemType(),
-//     initialOffset: monitor.getInitialSourceClientOffset(),
-//     currentOffset: monitor.getSourceClientOffset(),
-//     isDragging: monitor.isDragging(),
-//   };
-// };
-
-// const CustomDragLayerWithDnD = DragLayer(collectLayer)(CustomDragLayer);
-
-const CardWithDnD = DropTarget(typeCard, specTarget, collectTarget)(
-  DragSource(typeCard, specSource, collectSource)(Card),
-);
 
 class ReactSortable extends Component {
   state = {
-    cards: [
-      {
-        id: 1,
-        title: '#1 장보기',
-        color: '#c6c1ff',
-      },
-      {
-        id: 2,
-        title: '#2 코딩하기',
-        color: '#aaf7ff',
-      },
-      {
-        id: 3,
-        title: '#3 요리하기',
-        color: '#ffe2cf',
-      },
-      {
-        id: 4,
-        title: '#4 청소하기',
-        color: '#f6ffcb',
-      },
-      {
-        id: 5,
-        title: '#5 빨래하기',
-        color: '#d7ffcf',
-      },
-    ],
+    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
   };
-
-  moveCard = (id, index) => {
-    const { cards } = this.state;
-    const sourceCard = cards.find(card => card.id === id);
-    const sortCards = cards.filter(card => card.id !== id);
-
-    sortCards.splice(index, 0, sourceCard);
-    this.setState({ cards: sortCards });
+  onSortEnd = ({ oldIndex, newIndex }) => {
+    this.setState({
+      items: arrayMove(this.state.items, oldIndex, newIndex),
+    });
   };
-
   render() {
-    const { cards } = this.state;
-    return (
-      <div>
-        {cards.map((card, i) => {
-          const { id } = card;
-          return (
-            <CardWithDnD
-              key={id}
-              index={i}
-              moveCard={this.moveCard}
-              {...card}
-            />
-          );
-        })}
-      </div>
-    );
+    return <SortableList
+      items={this.state.items}
+      onSortEnd={this.onSortEnd}
+      transitionDuration={200}
+    />;
   }
 }
 
-export default DragDropContext(HTML5Backend)(ReactSortable);
+export default ReactSortable;
