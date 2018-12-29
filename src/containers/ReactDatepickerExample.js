9d4b7f617668f437647b883163f3c36116c86977
ahribori <ahribori@gmail.com>
2018-12-29 19:43:56 +0900

    React Day Picker 사용해보기

--- src/containers/ReactDatepickerExample.js
+++ src/containers/ReactDatepickerExample.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+
+/**
+ * React Datepicker
+ */
 import Datepicker, { registerLocale } from 'react-datepicker';
 // import ko from 'date-fns/locale/ko';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -7,15 +11,50 @@ import 'react-datepicker/dist/react-datepicker.css';
 //
 // console.log(ko)
 
+/**
+ * React Day Picker
+ */
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+const MONTHS = [
+  '1월',
+  '2월',
+  '3월',
+  '4월',
+  '5월',
+  '6월',
+  '7월',
+  '8월',
+  '9월',
+  '10월',
+  '11월',
+  '12월',
+];
+const WEEKDAYS_LONG = [
+  '일요일',
+  '월요일',
+  '화요일',
+  '수요일',
+  '목요일',
+  '금요일',
+  '토요일',
+];
+const WEEKDAYS_SHORT = ['일', '월', '화', '수', '목', '금', '토'];
+
+/**
+ * React Datetime
+ */
+
 class ReactDatepickerExample extends Component {
   state = {
     date: new Date(),
   };
 
   handleChange = selectedDate => {
-    console.log(selectedDate)
+    console.log(selectedDate);
     this.setState({ date: selectedDate });
   };
+
   render() {
     const { date } = this.state;
     return (
@@ -26,6 +65,17 @@ class ReactDatepickerExample extends Component {
           onChange={this.handleChange}
           selected={date}
           // locale={'ko'}
+        />
+
+        <h1>React Day Picker Example</h1>
+        <DayPickerInput
+          onDayChange={this.handleChange}
+          value={date}
+          dayPickerProps={{
+            months: MONTHS,
+            weekdaysLong: WEEKDAYS_LONG,
+            weekdaysShort: WEEKDAYS_SHORT,
+          }}
         />
       </div>
     );
