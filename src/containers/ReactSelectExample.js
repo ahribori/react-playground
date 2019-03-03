416457c0f6a24bcc61298c711dc02f9dc738cc77
ahribori <ahribori@gmail.com>
2019-03-04 00:58:31 +0900

    RC-select가 position: fixed 블럭 안에 있을 때 드롭다운이 따로 노는 이슈 해결

--- src/containers/ReactSelectExample.js
+++ src/containers/ReactSelectExample.js
@@ -46,49 +46,68 @@ class ReactSelectExample extends Component {
       return null;
     }
     return (
-      <div style={{ margin: 20 }}>
-        <h2>controlled Select</h2>
-        <div style={{ width: 300 }}>
-          <Select
-            id="my-select"
-            value={value}
-            placeholder="placeholder"
-            dropdownMenuStyle={{ maxHeight: 200 }}
-            style={{ width: 500 }}
-            onBlur={this.onBlur}
-            onFocus={this.onFocus}
-            open={open}
-            optionLabelProp="children"
-            optionFilterProp="text"
-            onChange={this.onChange}
-            onDropdownVisibleChange={this.onDropdownVisibleChange}
-          >
-            <Option value="01" text="jack" title="jack">
-              <b
-                style={{
-                  color: 'red',
+      <div
+        style={{
+          height: 5000,
+        }}
+      >
+        <div
+          style={{
+            position: 'fixed',
+            top: 50,
+            left: 300,
+            border: '1px dashed black',
+          }}
+          id="dropdown-container"
+        >
+          <div style={{ margin: 20 }}>
+            <h2>controlled Select</h2>
+            <div style={{ width: 300 }}>
+              <Select
+                id="my-select"
+                value={value}
+                placeholder="placeholder"
+                dropdownMenuStyle={{ maxHeight: 200 }}
+                style={{ width: 500 }}
+                onBlur={this.onBlur}
+                onFocus={this.onFocus}
+                open={open}
+                optionLabelProp="children"
+                optionFilterProp="text"
+                onChange={this.onChange}
+                onDropdownVisibleChange={this.onDropdownVisibleChange}
+                getPopupContainer={node => {
+                  return document.getElementById('dropdown-container');
                 }}
               >
-                jack
-              </b>
-            </Option>
-            <Option value="11" text="lucy">
-              lucy
-            </Option>
-            <Option value="21" disabled text="disabled">
-              disabled
-            </Option>
-            <Option value="31" text="yiminghe">
-              yiminghe
-            </Option>
-            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => {
-              return (
-                <Option key={i} value={i} text={String(i)}>
-                  {i}-text
+                <Option value="01" text="jack" title="jack">
+                  <b
+                    style={{
+                      color: 'red',
+                    }}
+                  >
+                    jack
+                  </b>
                 </Option>
-              );
-            })}
-          </Select>
+                <Option value="11" text="lucy">
+                  lucy
+                </Option>
+                <Option value="21" disabled text="disabled">
+                  disabled
+                </Option>
+                <Option value="31" text="yiminghe">
+                  yiminghe
+                </Option>
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => {
+                  return (
+                    <Option key={i} value={i} text={String(i)}>
+                      {i}-text
+                    </Option>
+                  );
+                })}
+              </Select>
+            </div>
+          </div>
         </div>
       </div>
     );
